21a6707947ff319ca333a85d83d7235f697f89d4
오종진 <45455262+Oh-JongJin@users.noreply.github.com>
2021-05-17 16:40:38 +0900

    Add files via upload

---
+++ Doge.bat
@@ -0,0 +1,6 @@
+REM IMPORTANT: Replace DOGECOIN with your own DOGE wallet address after DOGE: And before the . (Worker1 is the name of the miner)
+PhoenixMiner.exe -pool ethash.unmineable.com:3333 -wal DOGE:DHmAXutjf7riaJhdYYx2ay2UAVm1BAABEw.homePC#393u-8lgv -pass x
+
+
+pause
+
